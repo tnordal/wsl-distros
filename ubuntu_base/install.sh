cda54554303ec4d0a406ab21e537a69bebe8b842
Tom Nordal <tnordal@gmail.com>
2022-11-13 09:41:03 +0100

    add base software

--- ubuntu_base/install.sh
+++ ubuntu_base/install.sh
@@ -5,3 +5,15 @@
 
 # Update system
 sudo apt update && sudo apt upgrade -y
+
+# Install base software
+sudo apt install -y curl \
+    git-core \
+    nano \
+    wget \
+    iputils-ping \
+    iproute2 \
+    mc \
+    rsync \
+    zsh
+
